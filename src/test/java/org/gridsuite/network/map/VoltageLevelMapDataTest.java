f2118e5b638c9442ead5563d6ee03683e1bb7ca8
Franck LECUYER <franck.lecuyer@rte-france.com>
2020-12-15 13:26:00 +0100

    Add licensing and authoring
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/test/java/org/gridsuite/network/map/VoltageLevelMapDataTest.java
+++ src/test/java/org/gridsuite/network/map/VoltageLevelMapDataTest.java
@@ -1,9 +1,18 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.network.map;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.gridsuite.network.map.model.VoltageLevelMapData;
 import org.junit.Test;
 
+/**
+ * @author Franck Lecuyer <franck.lecuyer at rte-france.com>
+ */
 public class VoltageLevelMapDataTest {
 
     @Test
